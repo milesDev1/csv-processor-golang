aeb35b4d441d431fccb50d9b898edf7ea94be77d
milesDev1 <115670603+milesDev1@users.noreply.github.com>
2023-04-11 10:49:19 +0100

    Initial GO File commit

---
+++ csv-processor.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+type User struct {
+	FName string `json:"First Name [Required]"`
+	LName string `json:"Last Name [Required]"`
+	Email string `json:"Email Address [Required]"`
+}
+
+func main() {
+	file, err := os.Open("./usersheet2.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	bytes, _ := ioutil.ReadAll(file)
+
+	var users Users
+
+	json.Unmarshal(bytes, &users)
+
+	csvfile, err := os.OpenFile("output.csv", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer csvfile.Close()
+
+	logfile, err := os.OpenFile("./log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer logfile.Close()
+
+	log.SetOutput(logfile)
+
+	newwriter := csv.NewWriter(csvfile)
+
+	for i := 0; i < len(users.Users); i++ {
+		var temparray []string
+		temparray = append(temparray, users.Users[i].FName, users.Users[i].LName, users.Users[i].Email)
+		log.Print(temparray)
+		newwriter.Write(temparray)
+		newwriter.Flush()
+	}
+	fmt.Println(len(users.Users))
+
+}
